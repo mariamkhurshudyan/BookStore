235bf4cbdd45cffca4d5d35a3aa866acff04420b
Mariam Khurshudyan <mariam_khurshudyan@epam.com>
2021-12-27 13:53:31 +0400

    logo

---
+++ src/Components/UI/logo/logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className='logo-container'>
+            <a href='/'>
+                <img src='https://tympanus.net/Freebies/libre/images/logo.svg' alt='logo'/>
+            </a>
+        </div>
+    );
+};
+
+export default Logo;
--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,8 +1,10 @@
 import Search from "../../Components/Search/Search";
+//import Logo from "../../Components/UI/logo/logo";
 
 const Header = () => {
   return (
     <div>
+      {/*<Logo />*/}
       <Search />
     </div>
   );
