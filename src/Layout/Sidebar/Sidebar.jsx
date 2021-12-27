4ac24bb0dc4b8f5f30cd6f12f257212cbf903d4e
Noubar Kalost <noubar.kaloust@gmail.com>
2021-12-27 16:36:08 +0400

    sidebar css module

--- src/Layout/Sidebar/Sidebar.jsx
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -1,10 +1,10 @@
 import { SidebarData } from "./SidebarData";
 import { Link } from "react-router-dom";
-import "./Sidebar.css";
+import styles from "./Sidebar.module.css";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
+    <div className={styles.sidebar}>
       <ul>
         {SidebarData.map((value, key) => {
           return (
@@ -16,7 +16,7 @@ const Sidebar = () => {
               {value.sub &&
                 value.sub.map((category, index) => {
                   return (
-                    <div key={index} className="sub-category">
+                    <div key={index} className={styles.subCategory}>
                       <Link to={category.path} style={{ color: "#191e38" }}>
                         <span>{category.title}</span>
                       </Link>
