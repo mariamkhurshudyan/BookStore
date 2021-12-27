5c96316fd22cdc81e8fce22c4838636af7b0178c
Noubar Kalost <noubar.kaloust@gmail.com>
2021-12-27 18:06:43 +0400

    footer design

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -1,5 +1,13 @@
+import styles from "./Footer.module.css";
+import Logo from "../../Components/UI/logo/Logo";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
