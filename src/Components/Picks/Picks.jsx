5c4201b77e69f3b865d84b9722b3f9d260b3e5db
Noubar Kalost <noubar.kaloust@gmail.com>
2022-01-04 15:05:01 +0400

    book design

---
+++ src/Components/Picks/Picks.jsx
@@ -0,0 +1,10 @@
+import styles from "./Picks.module.css"
+const Picks = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+};
+
+export default Picks;
---
+++ src/Components/Picks/PicksData.js
@@ -0,0 +1,19 @@
+export const PicksData = [
+    {
+        title: 'Popeye Jul 2018',
+        text: 'Eu quo donec pellentesque, urna velit. Ultrices ante iaculis ligula praesent',
+        imgURL: ""
+    },{
+        title: 'Brutus Aug 2018',
+        text: 'Sapien eget, fermentum curabitur quis ut pharetra odio id, vitae eros eget maecenas, ullamcorper vitae.',
+        imgURL: ""
+    },{
+        title: 'Casa Aug 2018',
+        text: 'Curabitur quis ut pharetra odio id, vitae eros eget tristique maecenas, ullamcorper vitae.',
+        imgURL: ""
+    },{
+        title: 'SUPER: Jul 2018',
+        text: 'Eu quo donec pellentesque, urna velit. Ultrices ante iaculis ligula praesent',
+        imgURL: ""
+    },
+]
