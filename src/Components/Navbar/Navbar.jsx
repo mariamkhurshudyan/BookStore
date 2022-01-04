b90a8df98130fc4861a800766bfc9531de2eeaf5
Noubar Kalost <noubar.kaloust@gmail.com>
2022-01-04 22:59:42 +0400

    design fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 import styles from "./Navbar.module.css"
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className={styles.container}>
             <p>Books</p>
             <div>
-            <Link to=''>
+            <NavLink to=''>
                 <span className={styles.link}>Library</span>
-            </Link>
-            <Link to='picks'>
+            </NavLink>
+            <NavLink to='picks'>
                 <span className={styles.link}>Picks For You</span>
-            </Link>
+            </NavLink>
             </div>
         </div>
     );
