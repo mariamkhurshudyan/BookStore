137178a80f720876cd586f8b22d4f58cc72e2c03
Noubar Kalost <noubar.kaloust@gmail.com>
2022-01-05 02:38:01 +0400

    most read book part

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,17 +1,27 @@
 import styles from "./Profile.module.css"
 import Avatar from "../UI/Avatar/Avatar";
 import Title from "../UI/Title/Title";
+import {PicksData} from "../Picks/PicksData";
 
 const Profile = () => {
     return (
         <div className={styles.container}>
             <div className={styles.header}>
             <Avatar/>
-            <div className={styles.about}>
-            <Title headingText='Noubar' headingLevel='h4'/>
-            <p>Joined since 2021</p>
+                <div className={styles.about}>
+                    <Title headingText='Noubar' headingLevel='h4'/>
+                    <p>Joined since 2021</p>
+                </div>
             </div>
+            <div className={styles.analytics}>
+                <Title headingText='You have read 4 of 30 books in 2018' headingLevel='h3'/>
+                    <div className={styles.covers}>
+                        {PicksData.map(cover=>(
+                            <img src={cover.imgURL} alt='read'/>
+                        ))}
+                    </div>
             </div>
+
         </div>
     );
 };
