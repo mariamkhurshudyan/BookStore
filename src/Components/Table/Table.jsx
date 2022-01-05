e32fa2c1dc0e7f55f93b9f13740d88f401dd8ece
Noubar Kalost <noubar.kaloust@gmail.com>
2022-01-05 13:29:10 +0400

    book design for break points

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -5,22 +5,21 @@ import Title from "../UI/Title/Title";
 const Table = () => {
   return (
     <div className={styles.holder}>
-      <Title
-        headingText="Your favourite authors in this category"
-        headingLevel="h4"
-      />
+        <div className={styles.title}>
+             <Title headingText="Your favourite authors in this category" headingLevel="h4"/>
+        </div>
       <table className={styles.table}>
         <thead className={styles.head}>
           <tr className={styles.row}>
-            {tableHeader.map((value, index) => {
-              return <td className={styles[value.position]}>{value.title}</td>;
-            })}
+            {tableHeader.map((value, index) => (
+               <td key={index} className={styles[value.position]}>{value.title}</td>
+                ))}
           </tr>
         </thead>
         <tbody>
           {tableData.map((value, index) => {
             return (
-              <tr>
+              <tr className={styles.row}>
                 <td key={index}>{value.author}</td>
                 <td className={styles.center}>{value.timesRead}</td>
                 <td className={styles.right}>{value.nationality}</td>
