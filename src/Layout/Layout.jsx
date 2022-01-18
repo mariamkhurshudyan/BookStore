62cbaf07e9b5bcffbab6d2d5926675eb86b4296a
Noubar Kalost <noubar.kaloust@gmail.com>
2022-01-18 13:12:26 +0400

    Refactoring the API call to Home.jsx

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,24 +1,17 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { SideBar, Footer, Header } from "./index";
 import styles from "./Layout.module.css";
 import Profile from "../Components/Profile/Profile";
-import Loader from "../Components/UI/Loader/Loader";
-import {useDispatch, useSelector} from "react-redux";
-import {getPlanets} from "../store/ducks/planets";
 
 const Layout = ({ children }) => {
-    const {loading} = useSelector(state => state)
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(getPlanets())
-    }, [dispatch])
   return (
     <>
       <Header />
         <div className={styles.container}>
             <SideBar />
             <div className={styles.home}>
-                { loading ? <Loader/> : children}</div>
+                { children}
+            </div>
              <Profile/>
       </div>
       <Footer />
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,8 +2,22 @@ import Analytics from "../Components/Analytics/Analytics";
 import Navbar from "../Components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import StatusHeader from "../Components/StatusHeader/StatusHeader";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getPlanets} from "../store/ducks/planets";
+import Loader from "../Components/UI/Loader/Loader";
 
 const Home = () => {
+    const dispatch = useDispatch()
+    const {loading} = useSelector(state => state)
+    useEffect(() => {
+        dispatch(getPlanets())
+    }, [dispatch])
+
+    if(loading) {
+        return <Loader/>
+    }
+
   return (
     <>
         <StatusHeader />
